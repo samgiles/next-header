1a10f7214d31cc40770b19af71ac0cda994efd66
Matthew Andrews <matt@mattandre.ws>
2014-10-13 16:15:26 +0100

    Add more missing stuff

---
+++ main.js
@@ -0,0 +1,14 @@
+var Delegate = require('dom-delegate');
+var header = document.querySelector('.o-header');
+var delegate = new Delegate(header);
+
+delegate.on('click', '.o-header-button-js', function(event) {
+	event.preventDefault();
+	var targetPanel = event.target.getAttribute('data-target-panel');
+	var currentPanel = header.getAttribute('data-panel');
+	if (currentPanel !== targetPanel) {
+		header.setAttribute('data-panel', targetPanel);
+	} else {
+		header.removeAttribute('data-panel');
+	}
+});
